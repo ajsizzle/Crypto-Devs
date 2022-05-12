2945474b7bdfc963e203d3c68f0d6a569651b4a4
Alex Johnson <ajohnson2389@gmail.com>
2022-05-12 15:56:05 -0400

    folder clean-up

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config({ path: ".env" })
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL
 
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   solidity: "0.8.4",
@@ -12,5 +15,8 @@ module.exports = {
       url: ALCHEMY_API_KEY_URL,
       accounts: [RINKEBY_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 }
