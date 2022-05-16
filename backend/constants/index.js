c41ef8f06cec38e42df6f4acb8cc8c4d1835dee2
Alex Johnson <ajohnson2389@gmail.com>
2022-05-16 12:53:22 -0400

    Added OpenSea API to view and purchase NFT collection

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -1,6 +1,6 @@
 // Address of Whitelist Contract
 const WHITELIST_CONTRACT_ADDRESS = "0x1F26B8D3faa077B5226F642BF69d87Bb4dcebd6f"
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://whitelist-dapp-wine.vercel.app/"
+const METADATA_URL = "https://whitelist-dapp-wine.vercel.app/api/"
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
